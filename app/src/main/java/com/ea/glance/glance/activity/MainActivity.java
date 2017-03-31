31324ed07022aceb5b1b8583fb3dce631ef15341
yangzikang <986762402@qq.com>
2017-04-01 01:05:40 +0800

    change library gradle and package

--- app/src/main/java/com/ea/glance/glance/activity/MainActivity.java
+++ app/src/main/java/com/ea/glance/glance/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ea.glance.glance;
+package com.ea.glance.glance.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ea.glance.glance.R;
 
 public class MainActivity extends AppCompatActivity {
 
